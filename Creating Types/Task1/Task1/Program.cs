64dbd39922fc52d9554c1a0a5e5a5723fc9f9b06
AMBarodzich <sasha-brdich@mail.ru>
2017-02-27 09:56:22 +0300

    Creating Types

--- Creating Types/Task1/Task1/Program.cs
+++ Creating Types/Task1/Task1/Program.cs
@@ -2,57 +2,136 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
 
-namespace Task1
+namespace ConsoleApplication1
 {
-
-    public class Customer
+    class Task1
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            var customer1 = new Product() { Name = "Borodich" };
-            customer1.Orders.Add(new Order("Book"));
-            customer1.Orders.Add(new Order("Cat"));
-            customer1.Orders.Add(new Order("Beer"));
+            Customer customer = new Customer(1, "Ivan");
+            List<Product> products = new List<Product>();
+            products.Add(new Product("milk", 7));
+            products.Add(new Product("meat", 9));
+            products.Add(new Product("bread", 5));
+            Order order = new Order(products, customer);
+            Shop.serviceOrder(order);
+            Console.ReadKey();
+        }
 
-            var customer2 = new Product() { Name = "Sashas" };
-            customer2.Orders.Add(new Order("meat"));
-            customer2.Orders.Add(new Order("cheese"));
-            customer2.Orders.Add(new Order("oil"));
+    }
 
-            var customers = new List<Product>() { customer1, customer2 };
+    class Shop
+    {
+        public static void serviceOrder(Order order)
+        {
+            Console.WriteLine("Servicing the client: " + order.getCustomer().getId());
+            Console.WriteLine("writing the check...");
+            Console.WriteLine(order.ToString());
+        }
+    }
 
-            var orders = new List<Order>();
-            foreach (var customer in customers)
+    class Order
+    {
+        IList<Product> products;
+        private Customer customer;
+        private decimal cost;
+
+        public Order(IList<Product> products, Customer customer)
+        {
+            this.customer = customer;
+            this.products = new List<Product>();
+            foreach (Product p in products)
             {
-                Console.WriteLine(customer.Name,customer.Products);
-                Console.WriteLine("Orders:");
-                orders = customer.Orders;
-                while (orders.Count > 0)
-                {
-                    Console.WriteLine(orders[0].OrderNumber);
-                    orders.RemoveAt(0); 
-                }
+                this.products.Add(p);
             }
-
-            Console.WriteLine("\nClose?");
-            string e = Console.ReadLine();
+            calculateCost();
         }
-    }
 
-    public class Product
-    {
-        public string Name { get; set; }
-        public List<Order> Orders { get; set; } = new List<Order>();
-        public List<Order> Products { get; set; } = new List<Order>();
-    }
-
-    public class Order
-    {
-        public Order(string orderNumber)
+        public IList<Product> getProductsList()
         {
-            OrderNumber = orderNumber;
+            return this.products;
         }
-        public string OrderNumber { get; set; }
+
+        public Customer getCustomer()
+        {
+            return this.customer;
+        }
+
+        public decimal getCost()
+        {
+            return this.cost;
+        }
+
+        public void calculateCost()
+        {
+            decimal result = 0;
+            foreach (Product p in products)
+            {
+                result += p.getPrice();
+            }
+            this.cost = result;
+        }
+
+        public String ToString()
+        {
+            StringBuilder stb = new StringBuilder();
+            stb.Append("Customer:" + this.customer.getId() + ";" + this.customer.getName() + "\n");
+            stb.Append("Products:\n");
+            foreach (Product p in this.products)
+            {
+                stb.Append(p.getName() + ";" + p.getPrice() + ";\n");
+            }
+            stb.Append("Result cost:" + this.cost);
+            return stb.ToString();
+        }
+    }
+
+    class Customer
+    {
+        private int id;
+        private String name;
+        public Customer(int id, String name)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public String getName()
+        {
+            return this.name;
+        }
+    }
+
+    class Product
+    {
+        private String name;
+        private decimal price;
+        public Product(String name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+        public String getName()
+        {
+            return this.name;
+        }
+        public decimal getPrice()
+        {
+            return this.price;
+        }
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public void setPrice(decimal price)
+        {
+            this.price = price;
+        }
     }
 }
